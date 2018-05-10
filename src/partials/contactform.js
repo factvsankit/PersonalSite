f32746139369e69752676117e0b66e8195487f6e
Ankit Sigdel <mta.ankit@gmail.com>
2018-05-10 12:23:28 +0545

    no message

--- src/partials/contactform.js
+++ src/partials/contactform.js
@@ -9,12 +9,39 @@ const ContactForm = props => {
       name="ContactLeads"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
+      className="contact-form"
     >
       <p style={{ display: "none" }}>
         <input name="bot-field" />
       </p>
-      <input type="text" name="name" placeholder="Name*" required />
-      <button>Add your Startup</button>
+      <div className="form-row clearfix">
+        <div className="form-element fe-half">
+          <input type="text" name="name" placeholder="Name *" required />
+        </div>
+        <div className="form-element fe-half">
+          <input type="text" name="email" placeholder="Email *" required />
+        </div>
+        </div>
+        <div className="form-row clearfix">
+          <div className="form-element fe-half">
+            <input type="text" name="company" placeholder="Company" />
+          </div>
+          <div className="form-element fe-half">
+            <input type="text" name="phone" placeholder="Phone" />
+          </div>
+        </div>
+        <div className="form-row clearfix">
+          <div className="form-element">
+            <textarea type="text" name="project_overview" placeholder="Project Overview *" required />
+          </div>
+      </div>
+      <div className="form-row clearfix">
+        <div className="form-element" style={{
+          textAlign: 'center'
+        }}>
+          <button>Submit</button>
+        </div>
+      </div>
     </form>
   )
 }
